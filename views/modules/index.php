3850a28f2ed89428f55b3e43e1672109566f4da7
Edsel Carvajal <edsel.melendez.carv@gmail.com>
2018-02-13 22:54:14 -0600

    Inicializacion de produccion

---
+++ views/modules/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Work</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,600,700,900" rel="stylesheet">
+</head>
+
+<body>
+   
+   <header>
+      
+       <nav>
+          
+          <ul class="brand">
+            <li><i style="color: #4400F5;" class="fas fa-map-marker"></i><a href="index.php">SYMFHY</a></li>
+          </ul>
+          
+           <ul>
+                  <li>
+                    <a href="about.php">About</a>
+                  </li>
+                  <li>
+                    <a href="#">Terms</a>
+                  </li>
+                   <li>
+                    <a href="#">Privacy Policy</a>
+                   </li>
+                   
+               
+           </ul>    
+       </nav><!-- END NAV -->
+       
+       <div class="container">
+           <div class="item header-content">
+               <h2>Symfhy</h2>
+               <p class="text text-gray">
+                  Platform to earn money watching YouTube videos, you can collect many points and choose 6 digits at random, if these digits are valid, congratulations, you have won Paypal balance, which you can charge with the minimum payment of $ 0.02.
+               </p>
+               
+               <a class="btn" href="">Start working Now.</a>
+
+           </div>
+           
+           
+           <div class="item">
+              <img src="images/linn.gif" alt="">
+           </div>
+       </div>
+       
+   </header>
+    
+</body>
+
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+
+
+</html>
