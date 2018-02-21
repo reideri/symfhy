39f99f6a3c9725dc6ae5b6126337ce496d728e8a
Edsel Carvajal <edsel.melendez.carv@gmail.com>
2018-02-21 16:51:11 -0600

    Actualizacion meta Vp

--- panel.php
+++ panel.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title></title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap-grid.min.css">
     <link rel="stylesheet" href="css/panel.css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:300,700,800,900" rel="stylesheet">    
